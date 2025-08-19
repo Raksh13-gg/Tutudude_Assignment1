cbe9f9b09c986af7e526a7db0bd895dadd35d101
Rakshita <rakshita13092006@gmail.com>
2025-08-19 23:46:26 +0530

    Task2 updated

---
+++ Task2.py
@@ -0,0 +1,10 @@
+#taking inputs
+print("enter the first name:")
+first_name = input()
+print("enter the second name:")
+second_name = input()
+#concatenating names
+full_name = first_name + " " + second_name
+#Displaying the result
+print("Hello", full_name + "! Welcome to the Python program") 
+#print(f"Hello {first_name} {second_name}! Welcome to the Python program")
